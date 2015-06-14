e1e213575a4f4ed0e6e4dfb249401ac36029bf58
kicha93 <atomicjumpers@gmail.com>
2015-06-14 22:08:40 +0200

    Baza danych

---
+++ TurboBase/Przebiegi.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Przebiegi]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Uzytkownik] INT NULL, 
+    [Id_wniosku] INT NULL, 
+    [Sciezka] VARCHAR(50) NULL
+)
---
+++ TurboBase/Wnioski.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Wnioski]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Typ] VARCHAR(50) NULL, 
+    [Id_uzytkownika] VARCHAR(50) NULL, 
+    [Id_wniosku] VARCHAR(50) NULL, 
+    [Sciezka] VARCHAR(50) NULL, 
+    [Data] DATETIME NULL, 
+    [Stan] INT NULL
+)
